2562d81aa8bc591381d553cc56bc49a24b1c2db9
agusduha <agusnduha@gmail.com>
2023-05-16 15:00:08 -0300

    add deposit revert tests

--- test/integration/Grateful.deposit.test.ts
+++ test/integration/Grateful.deposit.test.ts
@@ -1,7 +1,7 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { depositFixture } from "../fixtures/fixtures";
+import { deploySystemFixture, depositFixture } from "../fixtures/fixtures";
 import { deposit } from "../fixtures/utils/deposit";
 import { BigNumber } from "ethers";
 
@@ -94,6 +94,76 @@ describe("Grateful", () => {
       await expect(tx)
         .to.emit(fundsModule, "FundsDeposited")
         .withArgs(giver.profileId, vaultId, DEPOSIT_AMOUNT, aprox);
+    });
+  });
+
+  describe("Deposit reverts", () => {
+    it("Should revert on zero amount when depositing", async () => {
+      const { fundsModule, vaultId, giver } = await loadFixture(
+        deploySystemFixture
+      );
+
+      const tx = fundsModule
+        .connect(giver.signer)
+        .depositFunds(giver.profileId, vaultId, 0);
+
+      await expect(tx).to.be.revertedWithCustomError(fundsModule, "ZeroAmount");
+    });
+
+    it("Should revert on inactive vault when depositing", async () => {
+      const { fundsModule, giver } = await loadFixture(deploySystemFixture);
+
+      const DEPOSIT_AMOUNT = ethers.utils.parseUnits("1", 18);
+
+      const vaultId = ethers.utils.formatBytes32String("invalid-vaultId");
+
+      const tx = fundsModule
+        .connect(giver.signer)
+        .depositFunds(giver.profileId, vaultId, DEPOSIT_AMOUNT);
+
+      await expect(tx).to.be.revertedWithCustomError(
+        fundsModule,
+        "InvalidVault"
+      );
+    });
+
+    it("Should revert on inactive profile when depositing", async () => {
+      const { fundsModule, vaultId, giver } = await loadFixture(
+        deploySystemFixture
+      );
+
+      const DEPOSIT_AMOUNT = ethers.utils.parseUnits("1", 18);
+
+      const profileId = ethers.utils.formatBytes32String("invalid-profileId");
+
+      const tx = fundsModule
+        .connect(giver.signer)
+        .depositFunds(profileId, vaultId, DEPOSIT_AMOUNT);
+
+      await expect(tx).to.be.revertedWithCustomError(
+        fundsModule,
+        "ProfileNotFound"
+      );
+    });
+
+    it("Should revert on insufficient allowance when depositing", async () => {
+      const { fundsModule, vault, vaultId, giver } = await loadFixture(
+        deploySystemFixture
+      );
+
+      const tokenAddress = await vault.asset();
+      const token = await ethers.getContractAt("ERC20", tokenAddress);
+      const decimals = await token.decimals();
+      const DEPOSIT_AMOUNT = ethers.utils.parseUnits("1", decimals);
+
+      const tx = fundsModule
+        .connect(giver.signer)
+        .depositFunds(giver.profileId, vaultId, DEPOSIT_AMOUNT);
+
+      await expect(tx).to.be.revertedWithCustomError(
+        fundsModule,
+        "InsufficientAllowance"
+      );
     });
   });
 });
