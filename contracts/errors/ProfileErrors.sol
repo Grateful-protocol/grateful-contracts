0e83666d60d002b72dd3dab61cafd41171a4d147
agusduha <agusnduha@gmail.com>
2023-05-17 11:11:50 -0300

    remove unused profile utils

--- contracts/errors/ProfileErrors.sol
+++ contracts/errors/ProfileErrors.sol
@@ -6,48 +6,6 @@ pragma solidity 0.8.17;
  */
 library ProfileErrors {
     /**
-     * @notice Error when providing a profile address that is not allowed by the system.
-     *
-     * Thrown in:
-     * - `ProfileUtil.validateExistenceAndGetProfile()`
-     *
-     * Cases:
-     * - `FundsModule.depositFunds()`
-     * - `SubscriptionsModule.subscribe()`
-     * - `SubscriptionsModule.unsubscribe()`
-     *
-     */
-    error InvalidProfile();
-
-    /**
-     * @notice Error when a token ID from a profile has no owner.
-     *
-     * Thrown in:
-     * - `ProfileUtil.validateExistenceAndGetProfile()`
-     *
-     * Cases:
-     * - `FundsModule.depositFunds()`
-     * - `SubscriptionsModule.subscribe()`
-     * - `SubscriptionsModule.unsubscribe()`
-     *
-     */
-    error NonExistentProfile();
-
-    /**
-     * @notice Error when a user is not approved or owner for a profile token ID.
-     *
-     * Thrown in:
-     * - `ProfileUtil.validateAllowanceAndGetProfile()`
-     *
-     * Cases:
-     * - `FundsModule.withdrawFunds()`
-     * - `SubscriptionsModule.subscribe()`
-     * - `SubscriptionsModule.unsubscribe()`
-     *
-     */
-    error UnauthorizedProfile();
-
-    /**
      * @notice Thrown when a profile attempts to renounce a permission that it didn't have.
      *
      * Cases:
