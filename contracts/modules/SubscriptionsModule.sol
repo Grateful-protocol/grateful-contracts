87b9b31b1d5e7a3315d1b70ea22b409427bfed79
agusduha <agusnduha@gmail.com>
2023-06-02 14:32:09 -0300

    refactor subscription utils

--- contracts/modules/SubscriptionsModule.sol
+++ contracts/modules/SubscriptionsModule.sol
@@ -11,8 +11,6 @@ import {Balance} from "../storage/Balance.sol";
 import {Subscription} from "../storage/Subscription.sol";
 import {SubscriptionRegistry} from "../storage/SubscriptionRegistry.sol";
 import {Fee} from "../storage/Fee.sol";
-import {IGratefulSubscription} from "../interfaces/IGratefulSubscription.sol";
-import {AssociatedSystem} from "@synthetixio/core-modules/contracts/storage/AssociatedSystem.sol";
 import {Profile} from "../storage/Profile.sol";
 import {ProfileRBAC} from "../storage/ProfileRBAC.sol";
 
@@ -25,11 +23,7 @@ contract SubscriptionsModule is ISubscriptionsModule {
     using Subscription for Subscription.Data;
     using SubscriptionRegistry for SubscriptionRegistry.Data;
     using Fee for Fee.Data;
-    using AssociatedSystem for AssociatedSystem.Data;
     using Profile for Profile.Data;
-
-    bytes32 private constant _GRATEFUL_SUBSCRIPTION_NFT =
-        "gratefulSubscriptionNft";
 
     /// @inheritdoc ISubscriptionsModule
     function subscribe(
@@ -48,11 +42,7 @@ contract SubscriptionsModule is ISubscriptionsModule {
 
         address owner = profile.rbac.owner;
 
-        Profile.exists(creatorId);
-
-        bytes32 treasuryId = Fee.load().gratefulFeeTreasury;
-        if (giverId == creatorId || creatorId == treasuryId)
-            revert SubscriptionErrors.InvalidCreator();
+        _validateCreator(giverId, creatorId);
 
         if (SubscriptionRegistry.load(giverId, creatorId).isSubscribed())
             revert SubscriptionErrors.AlreadySubscribed();
@@ -62,13 +52,8 @@ contract SubscriptionsModule is ISubscriptionsModule {
 
         uint256 rate = VaultUtil.getCurrentRate(vaultId, subscriptionRate);
 
-        (uint256 subscriptionId, uint256 feeRate, ) = _startSubscription(
-            giverId,
-            creatorId,
-            vaultId,
-            rate,
-            owner
-        );
+        (uint256 subscriptionId, uint256 feeRate, ) = SubscriptionUtil
+            .startSubscription(giverId, creatorId, vaultId, rate, owner);
 
         if (!Balance.load(giverId, vaultId).canStartSubscription())
             revert BalanceErrors.InsolventUser();
@@ -83,131 +68,6 @@ contract SubscriptionsModule is ISubscriptionsModule {
         );
     }
 
-    /**
-     * @dev Starts a subscription.
-     *
-     * This function is used from a user.
-     *
-     * Updates the balances flows (giver, creator and treasury).
-     *
-     * If the subscription between giver and creator already exist, then the subscription is
-     * updated to the new rate, else the subscription is created for the first time.
-     *
-     * To create a new subscription means to mint a new token ID from the Grateful subscription NFT.
-     */
-    function _startSubscription(
-        bytes32 giverId,
-        bytes32 creatorId,
-        bytes32 vaultId,
-        uint256 subscriptionRate,
-        address profileOwner
-    )
-        private
-        returns (uint256 subscriptionId, uint256 feeRate, uint256 totalRate)
-    {
-        // Calculate fee rate
-        feeRate = Fee.load().getFeeRate(subscriptionRate);
-
-        // Decrease giver flow
-        totalRate = subscriptionRate + feeRate;
-        Balance.load(giverId, vaultId).increaseOutflow(totalRate);
-
-        // Increase creator flow
-        Balance.load(creatorId, vaultId).increaseInflow(subscriptionRate);
-
-        // Increase treasury flow with feeRate
-        bytes32 treasuryId = Fee.load().gratefulFeeTreasury;
-        Balance.load(treasuryId, vaultId).increaseInflow(feeRate);
-
-        if (SubscriptionRegistry.load(giverId, creatorId).exists()) {
-            subscriptionId = _updateSubscription(
-                giverId,
-                creatorId,
-                vaultId,
-                subscriptionRate,
-                feeRate
-            );
-        } else {
-            subscriptionId = _createSubscription(
-                giverId,
-                creatorId,
-                vaultId,
-                subscriptionRate,
-                feeRate,
-                profileOwner
-            );
-        }
-    }
-
-    /**
-     * @dev Creates a subscription.
-     *
-     * This function is used from a user.
-     *
-     * Gets the next token ID from the subscription NFT.
-     *
-     * Saves the subscription data from this ID, and links it with the giver and creator.
-     *
-     * A new token ID is minted to the giver profile owner.
-     */
-    function _createSubscription(
-        bytes32 giverId,
-        bytes32 creatorId,
-        bytes32 vaultId,
-        uint256 subscriptionRate,
-        uint256 feeRate,
-        address profileOwner
-    ) private returns (uint256 subscriptionId) {
-        // Get subscription ID from subscription NFT
-        IGratefulSubscription gs = IGratefulSubscription(
-            AssociatedSystem.load(_GRATEFUL_SUBSCRIPTION_NFT).proxy
-        );
-        subscriptionId = gs.getCurrentTokenId();
-
-        // Start subscription
-        Subscription.Data storage subscription = Subscription.load(
-            subscriptionId
-        );
-        subscription.start(subscriptionRate, feeRate, creatorId, vaultId);
-
-        // Link subscription ID with subscription data
-        SubscriptionRegistry.load(giverId, creatorId).set(subscriptionId);
-
-        // Mint subscription NFT to giver profile owner
-        gs.mint(profileOwner);
-    }
-
-    /**
-     * @dev Updates a subscription.
-     *
-     * This function is used from a user.
-     *
-     * Loads the subscription data from the giver and creator.
-     *
-     * Updates the subscription data with the new rates and vault.
-     *
-     * No new subscription token is minted, the already created token is reused.
-     */
-    function _updateSubscription(
-        bytes32 giverId,
-        bytes32 creatorId,
-        bytes32 vaultId,
-        uint256 subscriptionRate,
-        uint256 feeRate
-    ) private returns (uint256 subscriptionId) {
-        // Get subscription data
-        subscriptionId = SubscriptionRegistry
-            .load(giverId, creatorId)
-            .subscriptionId;
-
-        Subscription.Data storage subscription = Subscription.load(
-            subscriptionId
-        );
-
-        // Update subscription
-        subscription.update(subscriptionRate, feeRate, vaultId);
-    }
-
     /// @inheritdoc ISubscriptionsModule
     function unsubscribe(bytes32 giverId, bytes32 creatorId) external {
         Profile.loadProfileAndValidatePermission(
@@ -215,16 +75,27 @@ contract SubscriptionsModule is ISubscriptionsModule {
             ProfileRBAC._UNSUBSCRIBE_PERMISSION
         );
 
-        Profile.exists(creatorId);
-
-        bytes32 treasuryId = Fee.load().gratefulFeeTreasury;
-        if (giverId == creatorId || creatorId == treasuryId)
-            revert SubscriptionErrors.InvalidCreator();
+        _validateCreator(giverId, creatorId);
 
         if (!SubscriptionRegistry.load(giverId, creatorId).isSubscribed())
             revert SubscriptionErrors.NotSubscribed();
 
         SubscriptionUtil.finishSubscription(giverId, creatorId);
+    }
+
+    /**
+     * @dev Validates if the creator is correct.
+     *
+     * - Only existing creator profile ID
+     * - Giver and creator cannot be the same
+     * - Creator cannot be Grateful treasury
+     */
+    function _validateCreator(bytes32 giverId, bytes32 creatorId) private view {
+        Profile.exists(creatorId);
+
+        bytes32 treasuryId = Fee.load().gratefulFeeTreasury;
+        if (giverId == creatorId || creatorId == treasuryId)
+            revert SubscriptionErrors.InvalidCreator();
     }
 
     /// @inheritdoc ISubscriptionsModule
--- contracts/utils/SubscriptionUtil.sol
+++ contracts/utils/SubscriptionUtil.sol
@@ -5,6 +5,10 @@ import {Balance} from "../storage/Balance.sol";
 import {Subscription} from "../storage/Subscription.sol";
 import {SubscriptionRegistry} from "../storage/SubscriptionRegistry.sol";
 import {Fee} from "../storage/Fee.sol";
+import {IGratefulSubscription} from "../interfaces/IGratefulSubscription.sol";
+import {AssociatedSystem} from "@synthetixio/core-modules/contracts/storage/AssociatedSystem.sol";
+import {Fee} from "../storage/Fee.sol";
+import {SubscriptionRegistry} from "../storage/SubscriptionRegistry.sol";
 
 /**
  * @title Utils for reusing subscriptions interactions.
@@ -14,6 +18,12 @@ import {Fee} from "../storage/Fee.sol";
 library SubscriptionUtil {
     using Balance for Balance.Data;
     using Subscription for Subscription.Data;
+    using AssociatedSystem for AssociatedSystem.Data;
+    using Fee for Fee.Data;
+    using SubscriptionRegistry for SubscriptionRegistry.Data;
+
+    bytes32 private constant _GRATEFUL_SUBSCRIPTION_NFT =
+        "gratefulSubscriptionNft";
 
     /**
      * @notice Emits the data from the finished subscription
@@ -32,6 +42,131 @@ library SubscriptionUtil {
         uint256 rate,
         uint256 feeRate
     );
+
+    /**
+     * @dev Starts a subscription.
+     *
+     * This function is used from a user.
+     *
+     * Updates the balances flows (giver, creator and treasury).
+     *
+     * If the subscription between giver and creator already exist, then the subscription is
+     * updated to the new rate, else the subscription is created for the first time.
+     *
+     * To create a new subscription means to mint a new token ID from the Grateful subscription NFT.
+     */
+    function startSubscription(
+        bytes32 giverId,
+        bytes32 creatorId,
+        bytes32 vaultId,
+        uint256 subscriptionRate,
+        address profileOwner
+    )
+        internal
+        returns (uint256 subscriptionId, uint256 feeRate, uint256 totalRate)
+    {
+        // Calculate fee rate
+        feeRate = Fee.load().getFeeRate(subscriptionRate);
+
+        // Decrease giver flow
+        totalRate = subscriptionRate + feeRate;
+        Balance.load(giverId, vaultId).increaseOutflow(totalRate);
+
+        // Increase creator flow
+        Balance.load(creatorId, vaultId).increaseInflow(subscriptionRate);
+
+        // Increase treasury flow with feeRate
+        bytes32 treasuryId = Fee.load().gratefulFeeTreasury;
+        Balance.load(treasuryId, vaultId).increaseInflow(feeRate);
+
+        if (SubscriptionRegistry.load(giverId, creatorId).exists()) {
+            subscriptionId = _updateSubscription(
+                giverId,
+                creatorId,
+                vaultId,
+                subscriptionRate,
+                feeRate
+            );
+        } else {
+            subscriptionId = _createSubscription(
+                giverId,
+                creatorId,
+                vaultId,
+                subscriptionRate,
+                feeRate,
+                profileOwner
+            );
+        }
+    }
+
+    /**
+     * @dev Creates a subscription.
+     *
+     * This function is used from a user.
+     *
+     * Gets the next token ID from the subscription NFT.
+     *
+     * Saves the subscription data from this ID, and links it with the giver and creator.
+     *
+     * A new token ID is minted to the giver profile owner.
+     */
+    function _createSubscription(
+        bytes32 giverId,
+        bytes32 creatorId,
+        bytes32 vaultId,
+        uint256 subscriptionRate,
+        uint256 feeRate,
+        address profileOwner
+    ) private returns (uint256 subscriptionId) {
+        // Get subscription ID from subscription NFT
+        IGratefulSubscription gs = IGratefulSubscription(
+            AssociatedSystem.load(_GRATEFUL_SUBSCRIPTION_NFT).proxy
+        );
+        subscriptionId = gs.getCurrentTokenId();
+
+        // Start subscription
+        Subscription.Data storage subscription = Subscription.load(
+            subscriptionId
+        );
+        subscription.start(subscriptionRate, feeRate, creatorId, vaultId);
+
+        // Link subscription ID with subscription data
+        SubscriptionRegistry.load(giverId, creatorId).set(subscriptionId);
+
+        // Mint subscription NFT to giver profile owner
+        gs.mint(profileOwner);
+    }
+
+    /**
+     * @dev Updates a subscription.
+     *
+     * This function is used from a user.
+     *
+     * Loads the subscription data from the giver and creator.
+     *
+     * Updates the subscription data with the new rates and vault.
+     *
+     * No new subscription token is minted, the already created token is reused.
+     */
+    function _updateSubscription(
+        bytes32 giverId,
+        bytes32 creatorId,
+        bytes32 vaultId,
+        uint256 subscriptionRate,
+        uint256 feeRate
+    ) private returns (uint256 subscriptionId) {
+        // Get subscription data
+        subscriptionId = SubscriptionRegistry
+            .load(giverId, creatorId)
+            .subscriptionId;
+
+        Subscription.Data storage subscription = Subscription.load(
+            subscriptionId
+        );
+
+        // Update subscription
+        subscription.update(subscriptionRate, feeRate, vaultId);
+    }
 
     /**
      * @dev Finishes a subscription.
